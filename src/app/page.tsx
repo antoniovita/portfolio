d23b23510d52edce49ef90854572cd8e2a116e37
antonio <antoniovitafonseca@gmail.com>
2025-02-22 20:50:11 -0300

    Melhorias e mudanças nos icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope as fasEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
+import { easeInOut, motion } from "framer-motion";
 
 const Home = () => {
   const [formData, setFormData] = useState({
@@ -15,10 +16,7 @@ const Home = () => {
   });
 
   const [icons, setIcons] = useState<any[]>([]);
-  const [movingStars, setMovingStars] = useState([]);
-  const [stars, setStars] = useState([]);
   const [hovered, setHovered] = useState(false);
-
 
   const technologies = [
     "/images/css3-plain.png",
@@ -46,38 +44,41 @@ const Home = () => {
     setIcons(generatedIcons);
   }, []);
 
-
-
   useEffect(() => {
-    const generateStars = () => {
-      const fixedStars = [];
-      const movingStars = [];
-      
-      for (let i = 0; i < 50; i++) {
-        fixedStars.push({
-          top: `${Math.random() * 100}%`,
-          left: `${Math.random() * 100}%`,
-        });
-      }
+    const canvas = document.getElementById("starCanvas") as HTMLCanvasElement;
+    const ctx = canvas?.getContext("2d");
 
-      // Gerar estrelas móveis
-      for (let i = 0; i < 10; i++) {
-        movingStars.push({
-          top: `${Math.random() * 100}%`,
-          left: `${Math.random() * 100}%`,
-        });
-      }
+    if (!canvas || !ctx) return;
 
-      setStars(fixedStars);
-      setMovingStars(movingStars);
-    };
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    generateStars();
+    const stars = Array.from({ length: 200 }, () => ({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.random() * 2,
+      speed: Math.random() * 0.5 + 0.2,
+    }));
+
+    function animateStars() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "white";
+
+      stars.forEach((star) => {
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 0.6);
+        ctx.fill();
+
+        star.y += star.speed;
+        if (star.y > canvas.height) star.y = 0;
+      });
+
+      requestAnimationFrame(animateStars);
+    }
+
+    animateStars();
   }, []);
 
-
-
-  // Handling input change
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
@@ -86,7 +87,6 @@ const Home = () => {
     });
   };
 
-  // Handling form submission
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form Submitted", formData);
@@ -94,10 +94,50 @@ const Home = () => {
 
   return (
     <div className="min-h-screen bg-transparent text-white flex flex-col relative overflow-auto">
-     
 
-     
-     <div className="absolute inset-0 floating-icons z-0">
+      
+      <canvas id="starCanvas" className="fixed top-0 left-0 w-full h-full z-0"></canvas>
+      
+      <div className="relative pt-20">
+        <motion.div initial={{ opacity:0}} whileInView={{ opacity: 1}} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="pt-20 relative flex flex-col items-center justify-center z-30 text-white mt-[100px] mb-[500px]">
+            <h1 className=" bg-black text-5xl font-bold mb-3">Hello, I'm <span className="text-yellow-300">Antonio Vita</span></h1>
+            <p className="bg-black text-md font-thin"> Computer Science Student at PUC-Rio & Full-Stack Developer</p>
+            <div className="z-40 p-5 flex flex-wrap gap-4">
+              <Link href={"/portuguese"}> <Image src={"/images/BR.png"} width={40} height={40} alt={"Portuguse"}></Image></Link>
+              <Link href={""}> <Image src={"/images/FR.png"} width={40} height={40} alt={"French"}></Image></Link>
+              <Link href={""}> <Image src={"/images/UM.png"} width={40} height={40} alt={"English"}></Image></Link>
+              <Link href={""}> <Image src={"/images/ES.png"} width={40} height={40} alt={"Spanish"}></Image></Link>
+              <Link href={""}> <Image src={"/images/IT.png"} width={40} height={40} alt={"Italian"}></Image></Link>
+            </div>
+            
+            
+            
+            <a href="#projects" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="bg-black duration-300 all-transition hover:text-white flex flex-col justify-center items-center text-gray-200">
+              <p className="font-thin"> View More </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className= {`m-1 lucide lucide-move-down all-transition duration-300 ${hovered ? 'translate-y-1' : ''}`}
+              >
+                <path d="M8 18L12 22L16 18"/>
+                <path d="M12 2V22"/> </svg>
+            </a>
+        </motion.div>
+      </div>
+
+
+      <motion.div initial={{ opacity:0}} whileInView={{ opacity: 1}} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}>
+          <div className="flex flex-col justify-center items-center mt-[200px] mb-[200px]"> 
+            <h1 className="text-4xl"> What about the <span className="text-yellow-300">technologies</span> I know? </h1>
+            
+            <div className="absolute inset-0 floating-icons z-0">
           {icons.map((icon, index) => (
             <Image
               key={index}
@@ -115,63 +155,14 @@ const Home = () => {
           ))}
         </div>
 
+          </div>
 
 
-     <div className="stars-background">
-        {/* Estrelas fixas */}
-        {stars.map((star, index) => (
-          <div
-            key={index}
-            className="star-fixed"
-            style={{
-              top: star.top,
-              left: star.left,
-            }}
-          />
-        ))}
-
-        {/* Estrelas móveis */}
-        {movingStars.map((star, index) => (
-          <div
-            key={index}
-            className="star-moving"
-            style={{
-              top: star.top,
-              left: star.left,
-            }}
-          />
-        ))}
-      </div>
-
-      <div className="relative pt-20">
-        <div className="pt-20 relative flex flex-col items-center justify-center z-30 text-white mt-[200px] mb-[500px]">
-            <h1 className=" bg-black text-5xl font-bold mb-3">Hello, I'm <span className="text-yellow-300">Antonio Vita</span></h1>
-            <p className="bg-black text-md mb-6 font-thin"> Computer Science Student at PUC-Rio & Full-Stack Developer</p>
-            <a href="#projects" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="bg-black duration-300 all-transition hover:text-white flex flex-col justify-center items-center text-gray-200">
-              <p className="font-thin"> View More </p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="1"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className= {`m-1 lucide lucide-move-down all-transition duration-300 ${hovered ? 'translate-y-1' : ''}`}
-              >
-                <path d="M8 18L12 22L16 18"/>
-                <path d="M12 2V22"/> </svg>
-                </a>
-        </div>
-
-
-      </div>
+      </motion.div>
 
 
 
-      <div className="swiper-container w-full mb-[500px] ml-[20px] mr-[20px]">
+      <motion.div initial={{ opacity:0}} whileInView={{ opacity: 1}} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="swiper-container w-full mb-[500px] ml-[20px] mr-[20px]">
         <Swiper
           spaceBetween={20}
           slidesPerView={3}
@@ -184,7 +175,7 @@ const Home = () => {
               <h1 className="text-2xl font-semibold">Lorem ipsum dolor sit amet.</h1>
               <p className="text-lg font-thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta possimus doloribus a vero ea delectus at error blanditiis? </p>
               <img
-                className="w-full h-80 object-cover rounded-xl shadow-lg mt-4"
+                className="w-full h-60 w-100 object-cover rounded-xl shadow-lg mt-4"
                 src="https://i.ytimg.com/vi/P8YuWEkTeuE/maxresdefault.jpg"
                 alt="Project 1"
               />
@@ -192,23 +183,23 @@ const Home = () => {
           </SwiperSlide>
 
         </Swiper>
-      </div>
+      </motion.div>
 
 
 
 
 
       <div className="flex items-center justify-center gap-[120px] z-30 mb-[300px]">
-          <Image width={400} height={400} className="border-gray-900 border-3 ml-[100px] rounded-full" src={"/images/carteirinha.jpg"} alt={""}/> 
-          <div className="mt-[40px] bg-black p-20 flex flex-col justify-center space-y-4 max-w-[700px]">
+         <motion.div  initial={{ opacity:0}} whileInView={{ opacity: 1}} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}><Image width={400} height={400} className="border-gray-900 border-3 ml-[100px] rounded-full" src={"/images/carteirinha.jpg"} alt={""}/></motion.div>
+          <motion.div className="mt-[40px] p-20 flex flex-col justify-center space-y-4 max-w-[700px]" initial={{ opacity:0}} whileInView={{ opacity: 1}} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}>
             <h1 className="text-4xl font-semibold text-white" >What about <span className="text-yellow-300">me</span>?</h1>
             <p className="text-lg font-light text-gray-300">I'm, a Computer Science student at PUC-Rio with a merit scholarship. I have 1 year of experience in web development, mainly with Typescript, React, Node.js, and databases like MongoDB and PostgreSQL. I’m also interested in blockchain development and always looking to learn more.</p>
-            <div className=" flex flex-wrap gap-[100px] justify-center">
+            <div className=" flex flex-wrap gap-[120px] justify-center">
               <Link className='mt-8' href={""}><FontAwesomeIcon icon={faGithub} size="3x" /></Link>
               <Link className='mt-8' href={""}><FontAwesomeIcon icon={faLinkedin} size="3x" /></Link>
               <Link className='mt-8' href={""}><FontAwesomeIcon icon={fasEnvelope} size="3x" /></Link>
             </div>
-          </div>
+          </motion.div>
 
         </div>
 
@@ -250,6 +241,7 @@ const Home = () => {
         </form>
         </div>
       </div>
+
     </div>
   );
 };
