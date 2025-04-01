d3e407ebab6bf0fa55eef32952410e94aa7564df
antonio <antoniovitafonseca@gmail.com>
2025-04-01 00:28:12 -0300

    swiper fixed

--- src/components/mywork.tsx
+++ src/components/mywork.tsx
@@ -7,37 +7,30 @@ import Image from 'next/image';
 const MyWork = () => {
   return (
     <div className="w-full max-w-[90rem] mx-auto py-20">
-      <h1 className="text-3xl md:text-4xl z-30 mb-[20px] text-center">
+      <h1 className="text-3xl md:text-4xl z-30 text-center">
         A little of my <span className="text-yellow-400">work</span>
       </h1>
 
       <Swiper
-        effect="coverflow"
         grabCursor={true}
-        centeredSlides={true}
+        centeredSlides={false} 
         loop={true}
-        spaceBetween={0}
-        slidesPerView={3}
+        spaceBetween={20} 
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
-        coverflowEffect={{
-          rotate: 0, 
-          stretch: 0,  
-          depth: 200,      
-          modifier: 2,      
-          slideShadows: true,
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          768: { slidesPerView: 2 }, 
+          1024: { slidesPerView: 3 }, 
         }}
         modules={[Autoplay, EffectCoverflow]}
         className="h-[550px]"
       >
         {[1, 2, 3, 4, 5].map((num) => (
-          <SwiperSlide
-            key={num}
-            className="transition-all duration-300 w-[400px]"
-          >
-            <div className="text-white flex items-center justify-center rounded-md text-xl h-full swiper-slide-content">
+          <SwiperSlide key={num} className="w-[400px]">
+            <div className="text-white flex items-center justify-center rounded-md text-xl h-full">
               <div className="flex flex-col gap-4 bg-black p-3">
                 <Image
                   alt="projects-images"
@@ -47,39 +40,22 @@ const MyWork = () => {
                   className="rounded-lg"
                 />
                 <div className="gap-2 flex flex-col">
-                  <h1 className="font-bold text-lg ">IRSA: Radiologia por Imagem</h1>
+                  <h1 className="font-normal text-lg">IRSA: Radiologia por Imagem</h1>
                   <p className="font-thin text-sm">
-                    projeto de SPA feito com next.js Tailwindcss
+                    Projeto de SPA feito com Next.js e TailwindCSS
                   </p>
                 </div>
                 <div className="flex flex-row flex-wrap gap-2">
                   <div className="rounded-full bg-blue-500 px-3 py-1 flex items-center gap-2">
-                    <Image
-                      alt="typescript"
-                      width={18}
-                      height={13}
-                      src="/images/typescript-plain.png"
-                    />
+                    <Image alt="typescript" width={18} height={13} src="/images/typescript-plain.png" />
                     <h1 className="text-white font-extralight text-sm">Typescript</h1>
                   </div>
-
                   <div className="rounded-full bg-teal-900 px-3 py-1 flex items-center gap-2">
-                    <Image
-                      alt="tailwind"
-                      width={18}
-                      height={13}
-                      src="/images/tailwindcss-plain.png"
-                    />
-                    <h1 className="text-white font-extralight text-sm">Tailwindcss</h1>
+                    <Image alt="tailwind" width={18} height={13} src="/images/tailwindcss-plain.png" />
+                    <h1 className="text-white font-extralight text-sm">TailwindCSS</h1>
                   </div>
-
                   <div className="rounded-full bg-gray-800 px-3 py-1 flex items-center gap-2">
-                    <Image
-                      alt="nextjs"
-                      width={18}
-                      height={13}
-                      src="/images/nextjs-original.png"
-                    />
+                    <Image alt="nextjs" width={18} height={13} src="/images/nextjs-original.png" />
                     <h1 className="text-white font-extralight text-sm">Next.js</h1>
                   </div>
                 </div>
@@ -88,13 +64,6 @@ const MyWork = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-
-      <style jsx global>{`
-        .swiper-slide-active .swiper-slide-content {
-          transform: scale(1.1);
-          transition: transform 0.3s ease;
-        }
-      `}</style>
     </div>
   );
 };
