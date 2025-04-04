dcc8fb08e792c582ea74d42178cbaa316242fe98
antonio <antoniovitafonseca@gmail.com>
2025-04-04 19:13:53 -0300

    idiomas atualizados menos o frances

--- src/app/spanish/page.tsx
+++ src/app/spanish/page.tsx
@@ -3,15 +3,17 @@ import { useState, useEffect, ChangeEvent, FormEvent, useMemo } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope as fasEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope as fasEnvelope, faXmark } from '@fortawesome/free-solid-svg-icons';
 import Link from "next/link";
 import { motion } from "framer-motion";
 import emailjs from '@emailjs/browser';
+import MyWork from "@/components/mywork";
 
 const Home = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
+  const [showStack, setShowStack] = useState(false);
 
   const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -52,7 +54,10 @@ const Home = () => {
     "/images/git-plain-wordmark.png",
     "/images/github-original.png",
     "/images/docker-plain.png",
-    "/images/java.png"
+    "/images/java.png",
+    "/images/html5-plain.png",
+    "/images/spring-boot.webp",
+    "/images/react-original.png",
   ], []);
 
   useEffect(() => {
@@ -102,6 +107,7 @@ const Home = () => {
   
     setIcons(generatedIcons);
   }, [technologies]);
+  
   useEffect(() => {
     const canvas = document.getElementById("starCanvas") as HTMLCanvasElement;
     const ctx = canvas?.getContext("2d");
@@ -146,7 +152,7 @@ const Home = () => {
       <div className="relative">
         <motion.div initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="relative flex flex-col items-center justify-center z-30 text-white h-screen px-4">
             <h1 className="bg-black text-4xl md:text-5xl font-bold mb-3 text-center">Hola, me llamo <span className="text-yellow-300">Antonio Vita</span></h1>
-            <p className="bg-black text-md font-thin text-center"> Estudiante de Informática en PUC-Rio & Desarrollador Full-Stack</p>
+            <p className="bg-black text-md font-thin text-center"> Estudiante de Ciencias de la Computación en PUC-Rio & Desarrollador Full-Stack </p>
             <div className="z-40 p-5 flex flex-wrap gap-4 justify-center">
               <Link href={"/portuguese"}> <Image src={"/images/BR.png"} width={40} height={40} alt={"Portuguse"} /></Link>
               <Link href={"/french"}> <Image src={"/images/FR.png"} width={40} height={40} alt={"French"} /></Link>
@@ -156,7 +162,7 @@ const Home = () => {
             </div>
             
             <a href="#about" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="bg-black duration-300 all-transition hover:text-white flex flex-col justify-center items-center text-gray-200">
-              <p className="font-thin"> Ver más </p>
+              <p className="font-thin"> Veer Más </p>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="18"
@@ -176,49 +182,94 @@ const Home = () => {
         </motion.div>
       </div>
 
-      <motion.div initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration:3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="px-4">
-          <div className="flex flex-col justify-center items-center h-screen relative"> 
-            <h1 className="text-3xl md:text-4xl bg-black z-30 mb-[5px] text-center"> ¿Cuales son las <span className="text-yellow-300">tecnologias</span> que utilizo?  </h1>
-            <p className="bg-black text-sm font-thin z-30 text-center"> TypeScript | React | Next.js | Node.js | JavaScript | Tailwindcss | MySQL | </p>
-            <p className="bg-black text-sm font-thin z-30 text-center"> PostgreSQL | MongoDB | Git | Python | Docker </p>
-            <div className="absolute inset-0 floating-icons z-0">
-              {icons.map((icon, index) => (
-                <Image
-                  key={index}
-                  src={icon.src}
-                  alt="Technology Icon"
-                  width={50}
-                  height={50}
-                  className="floating-icon"
-                  style={{
-                    top: `${icon.top}%`,
-                    left: `${icon.left}%`,
-                    animationDelay: icon.animationDelay,
-                  }}
-                />
-              ))}
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="px-4">
+  <div className="flex flex-col justify-center items-center h-screen relative"> 
+    <h1 className="text-3xl md:text-4xl z-30 mb-[5px] text-center bg-black rounded-full border border-gray-950 px-5 py-3"> ¿ Cual es mi <span className="text-yellow-300"> stack </span> ?</h1>
+    <button onClick={() => setShowStack(true)} className="bg-yellow-400 text-black rounded-full px-3 py-2 mt-5 font-normal hover:bg-yellow-900 hover:text-black all-transition duration-300"> Abrir la lista </button>
+    <div className="absolute inset-0 floating-icons z-0">
+      {icons.map((icon, index) => (
+        <Image
+          key={index}
+          src={icon.src}
+          alt="Technology Icon"
+          width={50}
+          height={50}
+          className="floating-icon"
+          style={{
+            top: `${icon.top}%`,
+            left: `${icon.left}%`,
+            animationDelay: icon.animationDelay,
+            width: '3vh',
+            height: '3vh'
+          }}
+        />
+      ))}
+    </div>
+  </div>
+
+  {showStack && (
+  <div className="fixed inset-0 flex justify-center items-center z-40">
+    <div className="bg-black w-[500px] h-[500px] relative flex flex-col justify-center items-center text-white p-5 rounded-lg shadow-lg border-gray-950 border">
+      <div className=" items-center flex flex-col gap-3 justify-start w-full overflow-y-scroll max-h-[350px] scrollbar-hide">
+        {technologies.map((tech, index) => {
+          const techNames = [
+            "CSS3", "Figma", "JavaScript", "Python", "TypeScript",
+            "PostgreSQL", "React", "Node.js", "MongoDB", "MySQL",
+            "Git", "GitHub", "Docker", "Java", "HTML5", "Spring Boot", "React Native"
+          ];
+          
+          return (
+            <div key={index} className="bg-gray-950 px-4 py-2 rounded-full flex flex-row gap-3 items-center w-[90%]">
+              <Image src={tech} alt={techNames[index]} width={50} height={50} className="w-6 h-6"/>
+              <h1 className="font-normal">{techNames[index]}</h1>
             </div>
-          </div>
+          );
+        })}
+      </div>
+      <button onClick={() => setShowStack(false)} className=" text-black bg-yellow-400 w-[90%] py-4 rounded-lg mt-5">
+        Volver
+      </button>
+    </div>
+  </div>
+)}
+</motion.div>
+
+
+      <motion.div initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration:3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="px-4">
+      <div className="flex flex-col justify-center items-center h-screen relative"> 
+        <MyWork></MyWork>
+        </div>
       </motion.div>
 
-      <div id="about" className="flex flex-col xl:flex-row sm:flex-col items-center justify-center gap-8 md:gap-20 z-30 h-screen px-4">
-        <motion.div initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}>
-          <Image width={300} height={300} className="border-gray-900 border-3 rounded-full" src={"/images/carteirinha.jpg"} alt={""}/>
-        </motion.div>
-        <motion.div className="mt-8 sm:mt-4 p-4 md:p-20 flex flex-col justify-center space-y-4 max-w-[700px]" initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}>
-          <h1 className="text-3xl md:text-4xl font-semibold text-white text-center md:text-left">¿Que puedo decir de <span className="text-yellow-300">mi</span>?</h1>
-          <p className="text-md md:text-lg font-light text-gray-300 text-center md:text-left">Soy estudiante de Informática en la PUC-Rio con una beca por mérito. Tengo 1 año de experiencia en desarrollo web, principalmente con Typescript, React, Node.js y bases de datos como MongoDB y PostgreSQL. También me interesa el desarrollo de blockchain y siempre estoy buscando aprender más.</p>
-          <div className="flex flex-wrap gap-8 justify-center md:justify-start">
-            <Link className='mt-8' href={"https://github.com/antoniovita"}><FontAwesomeIcon icon={faGithub} size="3x" /></Link>
-            <Link className='mt-8' href={"https://linkedin.com/in/antonio-vita-6177922b7"}><FontAwesomeIcon icon={faLinkedin} size="3x" /></Link>
-            <Link className='mt-8' href={""}><FontAwesomeIcon icon={fasEnvelope} size="3x" /></Link>
-          </div>
-        </motion.div>
-      </div>
+      <div id="about" className="flex flex-col xl:flex-row sm:flex-col items-center justify-center gap-6 md:gap-20 z-30 sm:h-auto md:h-screen px-6 sm:px-8 md:px-20">
+  <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}>
+    <Image width={250} height={250} className="border-gray-900 border-3 rounded-full sm:w-[280px] sm:h-[280px] md:w-[300px] md:h-[300px]" src={"/images/carteirinha.jpg"} alt={""} />
+  </motion.div>
+  <motion.div className="sm:mt-4 p-4 sm:p-10 md:p-20 flex flex-col justify-center space-y-4 max-w-[700px]" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }}>
+    <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-white text-center md:text-left">
+    ¿ Que puedo decir de <span className="text-yellow-300">mi </span>?
+    </h1>
+    <p className="text-sm sm:text-md md:text-lg font-light text-gray-300 text-center md:text-left">
+    Soy estudiante de Ciencias de la Computación en la PUC-Rio con una beca por mérito. Tengo un año de experiencia en desarrollo web, principalmente con TypeScript, React, Node.js y bases de datos como MongoDB y PostgreSQL. También me interesa el desarrollo en blockchain y siempre estoy buscando aprender más.
+    </p>
+    <div className="flex flex-wrap gap-6 justify-center md:justify-start">
+      <Link className="mt-6" href={"https://github.com/antoniovita"}>
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </Link>
+      <Link className="mt-6" href={"https://linkedin.com/in/antonio-vita-6177922b7"}>
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </Link>
+      <Link className="mt-6" href={""}>
+        <FontAwesomeIcon icon={fasEnvelope} size="2x" />
+      </Link>
+    </div>
+  </motion.div>
+</div>
 
-      <motion.div initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="h-screen justify-center flex flex-col items-center z-20 px-4">
-        <div className="bg-transparent flex flex-col items-center border border-gray-700 rounded-lg p-4 md:p-[45px] w-full sm:w-[90%] md:w-[70%] lg:w-[50%] xl:w-[40%]">
-          <h2 className="text-3xl md:text-4xl font-semibold mb-4 text-white text-center">Ponte en Contacto!</h2>
+
+      <motion.div initial={{ opacity:0 }} whileInView={{ opacity: 1 }} transition={{ duration: 3, ease: "easeOut" }} viewport={{ amount: 0.2 }} className="h-screen justify-center flex flex-col items-center z-20 px-8">
+        <div className="bg-transparent flex flex-col items-center border border-gray-950 rounded-lg p-4 md:p-[45px] w-full sm:w-[90%] md:w-[70%] lg:w-[50%] xl:w-[40%]">
+          <h2 className="text-3xl md:text-4xl font-semibold mb-4 text-white text-center">Ponte en Contacto</h2>
           <form onSubmit={sendEmail} className="w-full">
             <div className="mb-4">
               <label className="block text-md text-gray-300">Nombre</label>
@@ -226,9 +277,10 @@ const Home = () => {
                 type="text"
                 name="name"
                 value={name}
+                placeholder="Tu nombre..."
                 onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
                 required
-                className="mt-2 p-3 w-full border border-gray-600 rounded-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                className="mt-2 p-3 w-full font-normal rounded-2xl bg-gray-950 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
               />
             </div>
             <div className="mb-4">
@@ -237,23 +289,24 @@ const Home = () => {
                 type="email"
                 name="email"
                 value={email}
+                placeholder="Tu email..."
                 onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
                 required
-                className="mt-2 p-3 w-full border border-gray-600 rounded-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                className="mt-2 p-3 w-full font-normal rounded-2xl bg-gray-950 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
               />
             </div>
             <div className="mb-4">
               <label className="block text-md text-gray-300">Mensaje</label>
               <textarea
-                className="mt-2 p-3 w-full border border-gray-600 rounded-md bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                placeholder="Vuestro mensaje..."
+                className="mt-2 scrollbar-hide p-3 h-[150px] w-full rounded-2xl font-normal bg-gray-950 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="Tu mensaje..."
                 onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)}
                 value={message}
               ></textarea>
             </div>
             <button
               type="submit"
-              className="w-full bg-gray-700 text-white p-3 rounded-md mt-4 hover:bg-gray-600 transition-all duration-300"
+              className="w-full bg-yellow-400 text-black p-3 rounded-2xl mt-4 hover:bg-yellow-900 transition-all duration-300"
             >
               Enviar
             </button>
