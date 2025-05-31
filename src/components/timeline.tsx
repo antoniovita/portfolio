40933931bad04318493c5223d629e5a054c7f4c5
antonio vita <antoniovitafonseca@gmail.com>
2025-05-31 13:40:15 -0300

    responsivo ficou ruim

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -20,33 +20,25 @@ export default function Timeline({ timeline }: TimelineProps) {
   }, []);
 
   return (
-    <div className="max-w-5xl mx-auto py-16 z-40 items-center flex flex-col bg-transparent px-4 sm:px-6">
+    <div className="max-w-5xl mx-auto py-16 z-40 items-center flex flex-col bg-transparent">
       <div className="relative w-full">
-        <div className="hidden sm:block absolute left-1/2 transform -translate-x-1/2 h-full w-1 bg-gray-700" />
-
-        <div className="flex flex-col gap-[150px] sm:gap-[200px] relative">
+        <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-1 bg-gray-700" />
+        <div className="flex flex-col gap-[200px] relative">
           {timeline.map((item, index) => {
             const isLeft = index % 2 === 0;
-
             return (
               <div
                 key={index}
-                className="w-full flex flex-col sm:flex-row sm:justify-between items-center relative"
+                className="w-full flex justify-between items-center relative"
                 data-aos="fade-up"
               >
-                <div className="absolute left-1/2 transform -translate-x-1/2 w-5 h-5 bg-yellow-400 rounded-full border-4 border-black z-10 hidden sm:block" />
-
-                <div
-                  className={`sm:w-1/2 w-full px-4 sm:px-6 text-center sm:text-left ${
-                    isLeft ? 'sm:text-right' : 'sm:text-left sm:order-2'
-                  }`}
-                >
+                <div className="absolute left-1/2 transform -translate-x-1/2 w-5 h-5 bg-yellow-400 rounded-full border-4 border-black z-10" />
+                <div className={`w-1/2 px-6 ${isLeft ? 'text-right' : 'text-left'} ${isLeft ? '' : 'order-2'}`}>
                   <span className="text-yellow-400 text-sm block mb-1">{item.year}</span>
                   <h3 className="text-xl font-semibold text-white mb-3">{item.title}</h3>
                   <p className="text-gray-400 text-sm">{item.description}</p>
                 </div>
-
-                <div className="sm:w-1/2 hidden sm:block" />
+                <div className="w-1/2" />
               </div>
             );
           })}
